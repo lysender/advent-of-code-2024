c75fe54fb179912f88b560ab48556244384581e9
Lysender <lysender@darkstar-win>
2024-12-05 00:17:09 +0800

    fix: move all puzzles into a sub package

---
+++ day05/src/bin/solve.rs
@@ -0,0 +1,19 @@
+use day05::{part1, part2};
+use input::get_puzzle_input;
+
+fn main() {
+    run_part1();
+    run_part2();
+}
+
+pub fn run_part1() {
+    let input_string = get_puzzle_input("00");
+    let result = part1(input_string.as_str());
+    println!("Result: {}", result);
+}
+
+pub fn run_part2() {
+    let input_string = get_puzzle_input("00");
+    let result = part2(input_string.as_str());
+    println!("Result: {}", result);
+}
