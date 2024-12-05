9a3b55220300452b7baae4e18d4fd504a6b25c21
Lysender <lysender@darkstar-win>
2024-12-05 16:39:22 +0800

    fix: try day 5 part 2

--- day05/src/lib.rs
+++ day05/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use nom::{
     character::complete::{self, char, line_ending},
     multi::separated_list1,
@@ -10,7 +12,7 @@ pub fn part1(data: &str) -> i32 {
 }
 
 pub fn part2(data: &str) -> i32 {
-    solve_puzzle(data)
+    solve_puzzle2(data)
 }
 
 #[derive(Debug)]
@@ -33,6 +35,86 @@ fn solve_puzzle(data: &str) -> i32 {
         return result;
     }
     0
+}
+
+fn solve_puzzle2(data: &str) -> i32 {
+    let (rules, pages) = parse_rules(data);
+    if rules.len() > 0 && pages.len() > 0 {
+        let invalid_pages = collect_invalid_pages(&rules, &pages);
+        // println!("Invalid list: {:?}", invalid_pages);
+        let mut result: i32 = 0;
+
+        for i in 0..invalid_pages.len() {
+            let cur_pages = &invalid_pages[i];
+            let fixed = fix_invalid_pages(&rules, cur_pages);
+            // println!("Fixed: {:?}", fixed);
+            result += find_middle_val(&fixed);
+        }
+        return result;
+    }
+    0
+}
+
+fn collect_invalid_pages(rules: &Vec<Rule>, pages: &Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    let mut result: Vec<Vec<i32>> = Vec::new();
+    for i in 0..pages.len() {
+        let cur_pages = &pages[i];
+        if !valid_pages(rules, cur_pages) {
+            result.push(cur_pages.clone());
+        }
+    }
+
+    result
+}
+
+fn fix_invalid_pages(rules: &Vec<Rule>, pages: &Vec<i32>) -> Vec<i32> {
+    let mut result = pages.clone();
+    let map = make_map(pages);
+
+    let mut left_valid = false;
+    let mut breaker = 0;
+    while !left_valid {
+        // println!("Reset from top");
+        let mut valid_count = 0;
+        for i in 0..pages.len() {
+            // Find the broken element
+            if let Err(pos) = valid_left_rules(i, rules, pages, &map) {
+                // println!("LEFT: Broken page: {} at {}", pages[pos], pos);
+                // Move broken value to the right until it becomes valid or at the end
+                let mut is_broken = true;
+                let mut cur_pos = pos;
+                while is_broken && cur_pos < pages.len() - 1 {
+                    // println!("{:?}", result);
+                    // println!("swap values to the right");
+                    let tmp = result[cur_pos];
+                    result[cur_pos] = result[cur_pos + 1];
+                    result[cur_pos + 1] = tmp;
+                    cur_pos += 1;
+
+                    is_broken = valid_left_rules(i, rules, pages, &map).is_err();
+                }
+            } else {
+                valid_count += 1;
+            }
+        }
+        // println!("valid_count: {}", valid_count);
+        left_valid = valid_count == pages.len();
+
+        breaker += 1;
+        if breaker > 100 {
+            // println!("breaker...");
+            break;
+        }
+    }
+
+    for i in 1..pages.len() {
+        // Find broken element
+        if let Err(pos) = valid_right_rules(i, rules, pages, &map) {
+            // println!("RIGHT: Broken page: {} at {}", pages[pos], pos);
+        }
+    }
+
+    result
 }
 
 fn parse_rules(data: &str) -> (Vec<Rule>, Vec<Vec<i32>>) {
@@ -84,23 +166,43 @@ fn find_middle_val(pages: &Vec<i32>) -> i32 {
     pages[index]
 }
 
+fn make_map(pages: &Vec<i32>) -> HashMap<i32, usize> {
+    let mut map = HashMap::new();
+    for (k, v) in pages.iter().enumerate() {
+        map.insert(*v, k);
+    }
+    map
+}
+
 fn valid_pages(rules: &Vec<Rule>, pages: &Vec<i32>) -> bool {
+    let map = make_map(pages);
     for i in 0..pages.len() {
-        if !valid_page(i, rules, pages) {
+        if !valid_page(i, rules, pages, &map) {
             return false;
         }
     }
     true
 }
 
-fn valid_page(index: usize, rules: &Vec<Rule>, pages: &Vec<i32>) -> bool {
-    valid_left_rules(index, rules, pages) && valid_right_rules(index, rules, pages)
+fn valid_page(
+    index: usize,
+    rules: &Vec<Rule>,
+    pages: &Vec<i32>,
+    map: &HashMap<i32, usize>,
+) -> bool {
+    valid_left_rules(index, rules, pages, map).is_ok()
+        && valid_right_rules(index, rules, pages, map).is_ok()
 }
 
-fn valid_left_rules(index: usize, rules: &Vec<Rule>, pages: &Vec<i32>) -> bool {
+fn valid_left_rules(
+    index: usize,
+    rules: &Vec<Rule>,
+    pages: &Vec<i32>,
+    map: &HashMap<i32, usize>,
+) -> Result<(), usize> {
     if index <= 0 {
         // Nothing else to check beyond this point
-        return true;
+        return Ok(());
     }
 
     let val = pages[index];
@@ -113,20 +215,26 @@ fn valid_left_rules(index: usize, rules: &Vec<Rule>, pages: &Vec<i32>) -> bool {
         .collect();
 
     // Ensure that page doesn't violate anything to the left
-    let map = &pages[..index];
     for r in right_vals.iter() {
-        if let Some(_) = map.iter().find(|x| *x == r) {
-            return false;
+        if let Some(pos) = map.get(r) {
+            if pos < &index {
+                return Err(*pos);
+            }
         }
     }
 
-    true
+    Ok(())
 }
 
-fn valid_right_rules(index: usize, rules: &Vec<Rule>, pages: &Vec<i32>) -> bool {
+fn valid_right_rules(
+    index: usize,
+    rules: &Vec<Rule>,
+    pages: &Vec<i32>,
+    map: &HashMap<i32, usize>,
+) -> Result<(), usize> {
     if index >= pages.len() - 1 {
         // Nothing else to check beyond this point
-        return true;
+        return Ok(());
     }
 
     let val = pages[index];
@@ -139,14 +247,15 @@ fn valid_right_rules(index: usize, rules: &Vec<Rule>, pages: &Vec<i32>) -> bool 
         .collect();
 
     // Ensure that page doesn't violate anything to the right
-    let map = &pages[index..];
-    for r in left_vals.iter() {
-        if let Some(_) = map.iter().find(|x| *x == r) {
-            return false;
+    for l in left_vals.iter() {
+        if let Some(pos) = map.get(l) {
+            if &index < pos {
+                return Err(*pos);
+            }
         }
     }
 
-    true
+    Ok(())
 }
 
 #[cfg(test)]
@@ -172,7 +281,7 @@ mod tests {
     #[test]
     fn test_part2() {
         let input = get_puzzle_input("05-sample");
-        let result = solve_puzzle(input.as_str());
-        assert_eq!(result, 143);
+        let result = solve_puzzle2(input.as_str());
+        assert_eq!(result, 123);
     }
 }
