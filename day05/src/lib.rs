9e584a6f942b3ab9e538f8618685fb117e4bded8
Lysender <lysender@darkstar-win>
2024-12-05 20:19:00 +0800

    fix: improved part 1 performance and structure

--- day05/src/lib.rs
+++ day05/src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use nom::{
     character::complete::{self, char, line_ending},
@@ -21,14 +21,135 @@ struct Rule {
     right: i32,
 }
 
+#[derive(Debug)]
+struct PageRule {
+    rights: HashSet<i32>,
+    lefts: HashSet<i32>,
+}
+
+impl PageRule {
+    fn new() -> Self {
+        PageRule {
+            rights: HashSet::new(),
+            lefts: HashSet::new(),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct PageRuleMap {
+    map: HashMap<i32, PageRule>,
+    rules: Vec<Rule>,
+}
+
+impl PageRuleMap {
+    fn new(rules: Vec<Rule>) -> Self {
+        PageRuleMap {
+            map: HashMap::new(),
+            rules,
+        }
+    }
+
+    fn register_rules(&mut self, page: i32) {
+        if self.map.get(&page).is_some() {
+            return;
+        }
+
+        let mut page_rules = PageRule::new();
+        let rights: Vec<i32> = self
+            .rules
+            .iter()
+            .filter_map(|r| {
+                if r.left == page {
+                    return Some(r.right);
+                }
+                None
+            })
+            .collect();
+        let lefts: Vec<i32> = self
+            .rules
+            .iter()
+            .filter_map(|l| {
+                if l.right == page {
+                    return Some(l.left);
+                }
+                None
+            })
+            .collect();
+
+        page_rules.rights = HashSet::from_iter(rights.into_iter());
+        page_rules.lefts = HashSet::from_iter(lefts.into_iter());
+
+        self.map.insert(page, page_rules);
+    }
+
+    fn valid_pages(&mut self, pages: &Vec<i32>) -> bool {
+        for i in 0..pages.len() {
+            if !self.valid_page(i, pages) {
+                return false;
+            }
+        }
+        true
+    }
+
+    fn valid_page(&mut self, index: usize, pages: &Vec<i32>) -> bool {
+        self.valid_lefts(index, pages).is_ok() && self.valid_rights(index, pages).is_ok()
+    }
+
+    fn valid_lefts(&mut self, index: usize, pages: &Vec<i32>) -> Result<(), usize> {
+        if index <= 0 {
+            // Nothing else to check beyond this point
+            return Ok(());
+        }
+
+        let val = pages[index];
+        self.register_rules(val);
+
+        // All values to the left must not be listed in the right rules
+        if let Some(page_rules) = self.map.get(&val) {
+            let buffer = &pages[..index];
+            for (k, v) in buffer.iter().enumerate() {
+                if page_rules.rights.contains(v) {
+                    return Err(k);
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn valid_rights(&mut self, index: usize, pages: &Vec<i32>) -> Result<(), usize> {
+        if index >= pages.len() - 1 {
+            // Nothing else to check beyond this point
+            return Ok(());
+        }
+
+        let val = pages[index];
+        self.register_rules(val);
+
+        // All values to the right must not be listed in the lefts rules
+        if let Some(page_rules) = self.map.get(&val) {
+            let buffer = &pages[(index + 1)..];
+            for (k, v) in buffer.iter().enumerate() {
+                if page_rules.lefts.contains(v) {
+                    return Err(k);
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
+
 fn solve_puzzle(data: &str) -> i32 {
     let (rules, pages) = parse_rules(data);
     if rules.len() > 0 && pages.len() > 0 {
+        let mut worker = PageRuleMap::new(rules);
         let mut result: i32 = 0;
 
         for i in 0..pages.len() {
             let cur_pages = &pages[i];
-            if valid_pages(&rules, cur_pages) {
+            if worker.valid_pages(cur_pages) {
                 result += find_middle_val(cur_pages);
             }
         }
