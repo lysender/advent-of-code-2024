5bf25241594a282bf3e34f8b0f73386d7390b13e
Lysender <lysender@darkstar-win>
2024-12-05 20:39:44 +0800

    fix: almost there

--- day05/src/lib.rs
+++ day05/src/lib.rs
@@ -161,79 +161,73 @@ fn solve_puzzle(data: &str) -> i32 {
 fn solve_puzzle2(data: &str) -> i32 {
     let (rules, pages) = parse_rules(data);
     if rules.len() > 0 && pages.len() > 0 {
-        let invalid_pages = collect_invalid_pages(&rules, &pages);
-        // println!("Invalid list: {:?}", invalid_pages);
+        let mut worker = PageRuleMap::new(rules);
         let mut result: i32 = 0;
 
-        for i in 0..invalid_pages.len() {
-            let cur_pages = &invalid_pages[i];
-            let fixed = fix_invalid_pages(&rules, cur_pages);
-            // println!("Fixed: {:?}", fixed);
-            result += find_middle_val(&fixed);
+        for i in 0..pages.len() {
+            let cur_pages = &pages[i];
+            if !worker.valid_pages(cur_pages) {
+                // Fix invalid page
+                let fixed = fix_invalid_pages(&mut worker, cur_pages);
+                println!("{:?}", fixed);
+                // Get middle value and add result
+                result += find_middle_val(&fixed);
+            }
         }
+
         return result;
     }
     0
 }
 
-fn collect_invalid_pages(rules: &Vec<Rule>, pages: &Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    let mut result: Vec<Vec<i32>> = Vec::new();
-    for i in 0..pages.len() {
-        let cur_pages = &pages[i];
-        if !valid_pages(rules, cur_pages) {
-            result.push(cur_pages.clone());
-        }
-    }
-
-    result
-}
-
-fn fix_invalid_pages(rules: &Vec<Rule>, pages: &Vec<i32>) -> Vec<i32> {
+fn fix_invalid_pages(worker: &mut PageRuleMap, pages: &Vec<i32>) -> Vec<i32> {
     let mut result = pages.clone();
-    let map = make_map(pages);
 
     let mut left_valid = false;
     let mut breaker = 0;
     while !left_valid {
         // println!("Reset from top");
+        // For each element, check if any of the items to the left are invalid
+        // If there is an invalid element found then:
+        //   Copy the invalid element and put it after the current element
+        //   Remove the invalid element
+        //   Start over
+        // Move on to the next element
         let mut valid_count = 0;
         for i in 0..pages.len() {
             // Find the broken element
-            if let Err(pos) = valid_left_rules(i, rules, pages, &map) {
-                // println!("LEFT: Broken page: {} at {}", pages[pos], pos);
-                // Move broken value to the right until it becomes valid or at the end
-                let mut is_broken = true;
-                let mut cur_pos = pos;
-                while is_broken && cur_pos < pages.len() - 1 {
-                    // println!("{:?}", result);
-                    // println!("swap values to the right");
-                    let tmp = result[cur_pos];
-                    result[cur_pos] = result[cur_pos + 1];
-                    result[cur_pos + 1] = tmp;
-                    cur_pos += 1;
-
-                    is_broken = valid_left_rules(i, rules, pages, &map).is_err();
+            match worker.valid_lefts(i, &result) {
+                Ok(_) => {
+                    valid_count += 1;
                 }
-            } else {
-                valid_count += 1;
+                Err(pos) => {
+                    let broken = result[pos];
+                    // Insert new element next to current
+                    result.insert(i + 1, broken);
+                    // Remove the invalid element
+                    result.remove(pos);
+                    // Start over
+                    break;
+                }
             }
         }
-        // println!("valid_count: {}", valid_count);
+
+        println!("valid_count: {}", valid_count);
         left_valid = valid_count == pages.len();
 
         breaker += 1;
         if breaker > 100 {
-            // println!("breaker...");
+            println!("breaker...");
             break;
         }
     }
 
-    for i in 1..pages.len() {
-        // Find broken element
-        if let Err(pos) = valid_right_rules(i, rules, pages, &map) {
-            // println!("RIGHT: Broken page: {} at {}", pages[pos], pos);
-        }
-    }
+    //for i in 1..pages.len() {
+    //    // Find broken element
+    //    if let Err(pos) = valid_right_rules(i, rules, pages, &map) {
+    //        // println!("RIGHT: Broken page: {} at {}", pages[pos], pos);
+    //    }
+    //}
 
     result
 }
@@ -285,98 +279,6 @@ fn find_middle_val(pages: &Vec<i32>) -> i32 {
     assert!(pages.len() % 2 != 0, "Pages must be odd");
     let index: usize = pages.len() / 2;
     pages[index]
-}
-
-fn make_map(pages: &Vec<i32>) -> HashMap<i32, usize> {
-    let mut map = HashMap::new();
-    for (k, v) in pages.iter().enumerate() {
-        map.insert(*v, k);
-    }
-    map
-}
-
-fn valid_pages(rules: &Vec<Rule>, pages: &Vec<i32>) -> bool {
-    let map = make_map(pages);
-    for i in 0..pages.len() {
-        if !valid_page(i, rules, pages, &map) {
-            return false;
-        }
-    }
-    true
-}
-
-fn valid_page(
-    index: usize,
-    rules: &Vec<Rule>,
-    pages: &Vec<i32>,
-    map: &HashMap<i32, usize>,
-) -> bool {
-    valid_left_rules(index, rules, pages, map).is_ok()
-        && valid_right_rules(index, rules, pages, map).is_ok()
-}
-
-fn valid_left_rules(
-    index: usize,
-    rules: &Vec<Rule>,
-    pages: &Vec<i32>,
-    map: &HashMap<i32, usize>,
-) -> Result<(), usize> {
-    if index <= 0 {
-        // Nothing else to check beyond this point
-        return Ok(());
-    }
-
-    let val = pages[index];
-
-    // When val is the left of the rule, we should not find any right values to the left
-    let right_vals: Vec<i32> = rules
-        .iter()
-        .filter(|rule| rule.left == val)
-        .map(|rule| rule.right)
-        .collect();
-
-    // Ensure that page doesn't violate anything to the left
-    for r in right_vals.iter() {
-        if let Some(pos) = map.get(r) {
-            if pos < &index {
-                return Err(*pos);
-            }
-        }
-    }
-
-    Ok(())
-}
-
-fn valid_right_rules(
-    index: usize,
-    rules: &Vec<Rule>,
-    pages: &Vec<i32>,
-    map: &HashMap<i32, usize>,
-) -> Result<(), usize> {
-    if index >= pages.len() - 1 {
-        // Nothing else to check beyond this point
-        return Ok(());
-    }
-
-    let val = pages[index];
-
-    // When val is the right of the rule, we should not find any left values to the right
-    let left_vals: Vec<i32> = rules
-        .iter()
-        .filter(|rule| rule.right == val)
-        .map(|rule| rule.left)
-        .collect();
-
-    // Ensure that page doesn't violate anything to the right
-    for l in left_vals.iter() {
-        if let Some(pos) = map.get(l) {
-            if &index < pos {
-                return Err(*pos);
-            }
-        }
-    }
-
-    Ok(())
 }
 
 #[cfg(test)]
