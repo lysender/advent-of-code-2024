00551244556be28aa8f2e1a82a12ba5b5e4c088d
Lysender <lysender@darkstar-win>
2024-12-06 11:00:29 +0800

    fix: prepare for day06

---
+++ day06/benches/benchmarks.rs
@@ -0,0 +1,18 @@
+use day06::{part1, part2};
+use input::get_puzzle_input;
+
+fn main() {
+    divan::main();
+}
+
+#[divan::bench]
+fn part1_bench() {
+    let input_string = get_puzzle_input("00");
+    part1(divan::black_box(input_string.as_str()));
+}
+
+#[divan::bench]
+fn part2_bench() {
+    let input_string = get_puzzle_input("00");
+    part2(divan::black_box(input_string.as_str()));
+}
