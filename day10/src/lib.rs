5bb0a9240352dc20c36320ec1453aaae21485c98
Lysender <lysender@darkstar-win>
2024-12-11 12:19:17 +0800

    fix: solved part 2

--- day10/src/lib.rs
+++ day10/src/lib.rs
@@ -7,7 +7,7 @@ pub fn part1(input: &str) -> i32 {
 }
 
 pub fn part2(input: &str) -> i32 {
-    solve_puzzle(input)
+    solve_puzzle_trails(input)
 }
 
 fn solve_puzzle(input: &str) -> i32 {
@@ -22,7 +22,21 @@ fn solve_puzzle(input: &str) -> i32 {
     trail_heads.len() as i32
 }
 
+fn solve_puzzle_trails(input: &str) -> i32 {
+    let grid = parse_data(input);
+    let mut trails: HashSet<Vec<IVec2>> = HashSet::new();
+
+    for start in grid.starts.iter() {
+        find_distinct_trails(&grid, start, &mut trails);
+    }
+
+    // For each starting position, find all trails that leads to an end of trail
+    trails.len() as i32
+}
+
 fn find_trail_heads(grid: &Grid, start: &IVec2, result: &mut HashSet<(IVec2, IVec2)>) {
+    // Use BFS (breadth first search) algorithm to find a trail
+    // by spreading evenly until the peak is found
     let vrow: Vec<bool> = vec![false; grid.max_y as usize + 1];
     let mut visited: Vec<Vec<bool>> = vec![vrow; grid.max_x as usize + 1];
     let mut queue: VecDeque<IVec2> = VecDeque::new();
@@ -56,6 +70,55 @@ fn find_trail_heads(grid: &Grid, start: &IVec2, result: &mut HashSet<(IVec2, IVe
                             }
                         }
                     }
+                }
+            }
+        }
+    }
+}
+
+fn find_distinct_trails(grid: &Grid, start: &IVec2, result: &mut HashSet<Vec<IVec2>>) {
+    // Use DFS (Depth first search) algorithm to find a trail
+    // by going deep first to capture the whole path
+    let vrow: Vec<bool> = vec![false; grid.max_y as usize + 1];
+    let mut visited: Vec<Vec<bool>> = vec![vrow; grid.max_x as usize + 1];
+    let mut paths: Vec<IVec2> = Vec::new();
+    paths.push(start.clone());
+    find_trails_inner(grid, &mut visited, start, &paths, result);
+}
+
+fn find_trails_inner(
+    grid: &Grid,
+    visited: &mut Vec<Vec<bool>>,
+    curr: &IVec2,
+    paths: &Vec<IVec2>,
+    result: &mut HashSet<Vec<IVec2>>,
+) {
+    if let Some(curr_val) = grid.find_item(curr.x, curr.y) {
+        // Mark as visited
+        visited[curr.x as usize][curr.y as usize] = true;
+
+        if *curr_val == 9 {
+            // Found a trail
+            result.insert(paths.clone());
+            return;
+        }
+
+        // Visit sorrounding nodes
+        let around: Vec<IVec2> = vec![
+            IVec2::new(0, 1),
+            IVec2::new(1, 0),
+            IVec2::new(0, -1),
+            IVec2::new(-1, 0),
+        ];
+
+        for next in around.iter() {
+            let pos = curr + next;
+            if let Some(next_val) = grid.find_item(pos.x, pos.y) {
+                // Candidate must be +1 greater than current value
+                if *next_val == (curr_val + 1) && *next_val <= 9 {
+                    let mut new_paths = paths.clone();
+                    new_paths.push(pos);
+                    find_trails_inner(grid, visited, &pos, &new_paths, result);
                 }
             }
         }
@@ -138,7 +201,7 @@ mod tests {
     #[test]
     fn test_part2() {
         let input = get_puzzle_input("10-sample");
-        let result = solve_puzzle(input.as_str());
+        let result = solve_puzzle_trails(input.as_str());
         assert_eq!(result, 81);
     }
 }
